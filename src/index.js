06fcc8db2a322188a2f71eff11d7dce5868c74e1
kochubey-kseniya <96043439+kochubey-kseniya@users.noreply.github.com>
2022-02-24 11:38:26 +0300

    refactor: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  module.exports = function check(str, bracketsConfig) {
+  let config = [];
+  let new_str = str.slice(0);
+  bracketsConfig.forEach(item => config.push(item.join('')));
+  let count = str.length;
+  while (count > 0) {
+    for (let key of config) {
+      if (new_str.includes(key)) new_str = new_str.replace(key, '');
+    }
+    count--;
+  }
+  return new_str.length === 0 ? true : false;
 }
